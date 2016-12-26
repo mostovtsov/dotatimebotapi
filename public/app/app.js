bb6d786364daacca3b3c275a4133ae3a56fb6695
IIIypik <mostovtsov@gmail.com>
2016-12-26 20:02:26 +0300

    working list

--- public/app/app.js
+++ public/app/app.js
@@ -1,11 +1,13 @@
 function HearsListController($scope, $http) {
-    
     $http.get('/hears').then(function successCallback(response) {
         $scope.hearsList = response.data;
     });
-}
+
+    $scope.getTextVariants = function (variantsArray) {
+        return variantsArray.join(', ');
+    };
+};
 
 var app = angular.module('hearsApp', [])
     .controller('HearsListController', ['$scope', '$http', HearsListController]);
-
-
+    
